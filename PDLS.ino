17f9507ed41fa75221e307922515da3e084acbfe
Thechopsee <74382107+Thechopsee@users.noreply.github.com>
2023-09-03 21:28:52 +0200

    ADD: Steering

--- PDLS.ino
+++ PDLS.ino
@@ -1,12 +1,24 @@
 #include <Keyboard.h>
+#include <Joystick.h>
+
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK,
+0, 0, // Button Count, Hat Switch Count
+false, false, false, // X and Y, but no Z Axis
+false, false, false, // No Rx, Ry, or Rz
+false, false, // No rudder or throttle
+false, false, true); // No accelerator, brake, or steering;
 
 #define THROTTLEBTN 4
 #define BRAKEBTN 5
 
 void setup() {
   Keyboard.begin();
+  Joystick.begin();
+  Serial.begin(9600);
+  //Joystick.setXAxisRange(0, 1023);
   pinMode(THROTTLEBTN, INPUT_PULLUP);
   pinMode(BRAKEBTN, INPUT_PULLUP);
+  pinMode(A0,INPUT);
   
 }
 bool rdt=false;
@@ -14,6 +26,12 @@ bool rdb=false;
 void loop() {
   byte buttonState = digitalRead(THROTTLEBTN);
   
+  int steValue=analogRead(A0);
+  //steValue=map(steValue,0,1023,0,255);
+  //popř Joystick.setSteering(steValue/4);
+  Serial.println(steValue);
+  Joystick.setSteering(steValue);
+
   if (buttonState == LOW) {
       Keyboard.press('w');
       rdt=true;
