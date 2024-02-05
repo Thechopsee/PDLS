602b7686bdf360a83d07ecd259419d20935d0fb1
Thechopsee <74382107+Thechopsee@users.noreply.github.com>
2024-02-05 11:15:19 +0100

    fix:small fixes

--- PDLS.ino
+++ PDLS.ino
@@ -5,21 +5,16 @@
 #include <Joystick.h>
 #include "button.hh"
 
+const int ELEMENT_COUNT_MAX = 12; //max number of digital pins used on board
+
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK,
-0, 0, // Button Count, Hat Switch Count
+ELEMENT_COUNT_MAX, 0, // Button Count, Hat Switch Count
 false, false, false, // X and Y, but no Z Axis
 false, false, false, // No Rx, Ry, or Rz
 false, false, // No rudder or throttle
 true, true, true); // No accelerator, brake, or steering;
 
-const int ELEMENT_COUNT_MAX = 12;
 
-#define THROTTLEBTN 7
-#define BRAKEBTN 11
-#define CLUTCHBTN 10
-#define F5 3
-#define F6 9
-#define ENTERBTN 5
 
 Vector<SWButton*> swbuttons;
 
@@ -27,35 +22,52 @@ void setup() {
   Keyboard.begin();
   Joystick.begin();
   Serial.begin(9600);
-
-  pinMode(THROTTLEBTN, INPUT_PULLUP);
-  pinMode(BRAKEBTN, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
+  for(int i=1;i<=ELEMENT_COUNT_MAX;i++)
+  {
+      pinMode(i, INPUT_PULLUP);
+  }
+  
+//  pinMode(2, INPUT_PULLUP);
+//  pinMode(3, INPUT_PULLUP);
+//  pinMode(4, INPUT_PULLUP);
+//  pinMode(5, INPUT_PULLUP);
+//  pinMode(6, INPUT_PULLUP);
+//  pinMode(7, INPUT_PULLUP);
+//  pinMode(8, INPUT_PULLUP);
+//  pinMode(9, INPUT_PULLUP);
+//  pinMode(10, INPUT_PULLUP);
+//  pinMode(11, INPUT_PULLUP);
+//  pinMode(12, INPUT_PULLUP);
 
   SWButton* storage_array[ELEMENT_COUNT_MAX];
   swbuttons.setStorage(storage_array);
-  //buttons.push_back(new SWButton(1,' '))
-  //buttons.push_back(new SWButton(2,' '))
-  swbuttons.push_back(new SWButton(3,(char)KEY_F5));
-  swbuttons.push_back(new SWButton(4,'d'));
-  swbuttons.push_back(new SWButton(ENTERBTN,KEY_RETURN));
+//  //buttons.push_back(new SWButton(1,' '))
+//  //buttons.push_back(new SWButton(2,' '))
+//  swbuttons.push_back(new SWButton(3,(char)KEY_F5));
+//  swbuttons.push_back(new SWButton(4,'d'));
+//  swbuttons.push_back(new SWButton(5,KEY_RETURN));
+//  //swbuttons.push_back(new SWButton(6,'e'));
+//  swbuttons.push_back(new SWButton(7,'w'));
+//  swbuttons.push_back(new SWButton(8,'c'));
+//  swbuttons.push_back(new SWButton(9,(char)KEY_F6));
+//  swbuttons.push_back(new SWButton(10,(char) 0x20));
+//  swbuttons.push_back(new SWButton(11,'s'));
+//  swbuttons.push_back(new SWButton(12,'a'));
+
+  //buttons.push_back(new SWButton(1,' ',&Joystick))
+  //buttons.push_back(new SWButton(2,' ',&Joystick))
+  swbuttons.push_back(new SWButton(3,(char)KEY_F5,&Joystick));
+  swbuttons.push_back(new SWButton(4,'d',&Joystick));
+  swbuttons.push_back(new SWButton(5,KEY_RETURN,&Joystick));
   //swbuttons.push_back(new SWButton(6,'e'));
-  swbuttons.push_back(new SWButton(THROTTLEBTN,'w'));
-  swbuttons.push_back(new SWButton(8,'c'));
-  swbuttons.push_back(new SWButton(9,(char)KEY_F6));
-  swbuttons.push_back(new SWButton(10,(char) 0x20));
-  swbuttons.push_back(new SWButton(12,'a'));
-  swbuttons.push_back(new SWButton(BRAKEBTN,'s'));
- 
+  swbuttons.push_back(new SWButton(7,'w',&Joystick));
+  swbuttons.push_back(new SWButton(8,'c',&Joystick));
+  swbuttons.push_back(new SWButton(9,(char)KEY_F6,&Joystick));
+  swbuttons.push_back(new SWButton(10,(char) 0x20,&Joystick));
+  swbuttons.push_back(new SWButton(11,'s',&Joystick));
+  swbuttons.push_back(new SWButton(12,'a',&Joystick));
+
+   
   pinMode(A0,INPUT);
   pinMode(A1,INPUT);
   pinMode(A2,INPUT);
@@ -65,15 +77,8 @@ void setup() {
   Joystick.setAcceleratorRange(0, 61);
   Joystick.setBrakeRange(0, 61);
 }
-bool rdt=false;
-bool rdb=false;
-bool rdc=false;
+
 void loop() {
-  //byte buttonState = digitalRead(THROTTLEBTN);
-  //byte clutchState=digitalRead(CLUTCHBTN);
-  //byte F5State=digitalRead(F5);
-  //byte F6State=digitalRead(F6);
-  //byte EnterState=digitalRead(ENTER);
   int steValue=analogRead(A0);
   int controll1=analogRead(A1);
   int controll2=analogRead(A2);
@@ -83,14 +88,13 @@ void loop() {
   Joystick.setAccelerator(power);
   int brake=analogRead(A5);
   brake=(brake-1023)*-1;
-  Joystick.setBrake(brake);
 
+  
+  Joystick.setBrake(brake);
   Joystick.setSteering(steValue);
-  Serial.println(brake);
   
   for (int i = 0; i < swbuttons.size(); ++i) 
   {
-        //Serial.println(i);
         swbuttons[i]->checkButton();
   }
   
--- button.hh
+++ button.hh
@@ -1,22 +1,27 @@
 #pragma once
 #include "Arduino.h"
+#include <Joystick.h>
+
 class SWButton{
 private:
   int pin;
   char key;
   bool rdt;
+  Joystick_ *Joystick;
 public:
-    SWButton(int pin,char key)
+    SWButton(int pin,char key,Joystick_ *Joystick)
     {
       this->pin=pin;
       this->key=key;
       this->rdt=false;
+      this->Joystick=Joystick;
     }
     void checkButton()
     {
       byte buttonState = digitalRead(pin);
       if (buttonState == LOW) {
-      Keyboard.press(key);
+      //Keyboard.press(key);
+      Joystick->pressButton(pin);
       rdt=true;
       }
       else
@@ -24,7 +29,8 @@ public:
         if(rdt)
         {
           rdt=false;
-          Keyboard.release(key);
+          //Keyboard.release(key);
+          Joystick->releaseButton(pin);
         }
       }
     }
